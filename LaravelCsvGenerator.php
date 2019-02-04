5e66eb4c3d3998d45d7f38fa6c3297040e8dfb3b
Eugene <37518271+Eugene-Melbourne@users.noreply.github.com>
2019-02-05 10:42:39 +1100

    LaravelCsvGenerator.php

---
+++ LaravelCsvGenerator.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Services\Generators;
+
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use function response;
+
+class LaravelCsvGenerator
+{
+
+    const DEFAULT_HTTP_HEADERS = [
+        "Content-type"        => "text/csv",
+        "Content-Disposition" => "attachment; filename=file.csv",
+        "Pragma"              => "no-cache",
+        "Cache-Control"       => "must-revalidate, post-check=0, pre-check=0",
+        "Expires"             => "0"
+    ];
+
+    private $headers     = null;
+    private $data        = null;
+    private $httpHeaders = null;
+
+
+    public function setHeaders(array $headers): self
+    {
+        $this->headers = $headers;
+
+        return $this;
+    }
+
+
+    public function setData(array $data): self
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+
+    public function setHttpHeaders(array $httpHeaders): self
+    {
+        $this->httpHeaders = $httpHeaders;
+
+        return $this;
+    }
+
+
+    public function renderStream(string $fileName = null): StreamedResponse
+    {
+        $fileName    = $fileName ?? 'file.csv';
+        $httpHeaders = $this->httpHeaders ?? self::DEFAULT_HTTP_HEADERS;
+
+        return response()->streamDownload(
+                function (): void {
+                // Output the generated Csv to Browser
+                $file = fopen('php://output', 'w');
+                fputcsv($file, $this->headers);
+                foreach ($this->data as $record) {
+                    fputcsv($file, $record);
+                }
+                fclose($file);
+            }, $fileName, $httpHeaders);
+    }
+
+
+    public function toString(): string
+    {
+        $res = implode(',', $this->headers) . "\n";
+        foreach ($this->data as $record) {
+            $res .= '"' . implode('","', $record) . '"' . "\n";
+        }
+
+        return $res;
+    }
+
+
+}
