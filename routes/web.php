81d528e78cd024b0b7acbf312badf9101f7be519
Eugene <37518271+Eugene-Melbourne@users.noreply.github.com>
2019-02-05 10:45:09 +1100

    example and test

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+// example and test
+
+Route::get('csv', function () {
+
+    $data    = [
+        [1, 2.1],
+        [3, "hi, there"],
+    ];
+    $headers = ['one', 'two'];
+
+    return (new \App\Services\Generators\CsvGenerator())
+        ->setHeaders($headers)
+        ->setData($data)
+        ->renderStream();
+});
+
+
+Route::get('csv-string', function () {
+
+    $httpHeaders = array(
+        "Content-type"        => "text/csv",
+        "Content-Disposition" => "attachment; filename=file.csv",
+        "Pragma"              => "no-cache",
+        "Cache-Control"       => "must-revalidate, post-check=0, pre-check=0",
+        "Expires"             => "0"
+    );
+
+    $data    = [
+        [1, 2.1],
+        [3, "hi, there"],
+    ];
+    $headers = ['one', 'two'];
+
+    $str = (new \App\Services\Generators\CsvGenerator())
+    ->setHeaders($headers)
+    ->setData($data)
+    ->toString();
+
+    return response()->make($str, 200, $httpHeaders);
+});
